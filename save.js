0b4e6a4f9420cefebeef409eca0d497bcd8543bc
tadashiy1012 <tadashi.y1012@gmail.com>
2016-05-09 20:26:19 +0900

    add file load func

--- save.js
+++ save.js
@@ -5,6 +5,9 @@ const os = remote.require('os');
 const path = require('path');
 const fs = require('fs');
 
+const dialog = remote.require('dialog');
+const browserWindow = remote.require('browser-window');
+
 const desktopDirName = 'Desktop';
 const textFileName = 'memo.txt';
 const homeDirPath = os.homedir();
@@ -12,6 +15,7 @@ const desktopDirPath = path.join(homeDirPath, desktopDirName);
 const textFilePath = path.join(desktopDirPath, textFileName);
 
 const saveButton = document.querySelector('#saveBtn');
+const loadButton = document.querySelector('#loadBtn');
 const textArea = document.querySelector('#text');
 
 function saveText() {
@@ -26,4 +30,25 @@ function saveText() {
     });
 }
 
+function loadText() {
+    let focusedWindow = browserWindow.getFocusedWindow();
+    dialog.showOpenDialog(focusedWindow, {
+        properties: ['openFile'],
+        filters: [{
+            name: 'テキストファイル',
+            extensions: ['txt']
+        }]
+    }, (files) => {
+        let str = '';
+        files.forEach((file) => {
+            console.log(file);
+            let buf = fs.readFileSync(file);
+            str += buf.toString();
+        });
+        console.log(str);
+        textArea.value = str;
+    });
+}
+
 saveButton.addEventListener('click', saveText, false);
+loadButton.addEventListener('click', loadText, false);
