6f7155273995408f9ba3ca9128f9e5701f450805
tadashiy1012 <tadashi.y1012@gmail.com>
2016-07-06 15:55:22 +0900

    modify index.js change electron start logic

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.on('window-all-closed', () => {
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
